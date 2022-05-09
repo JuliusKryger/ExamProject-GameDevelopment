1ba7c441d9338c76a425788c4440f1d93595bcb9
Julius Madsen <2330051117@protonmail.com>
2022-05-09 22:39:06 +0200

    Added input controls for camera movement

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -6,16 +6,42 @@ public class CameraController : MonoBehaviour
 {
     public float movementSpeed;
     public float movementTime;
+    public Vector3 newPosition;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        newPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        HandleMovementInput();
+    }
+
+    void HandleMovementInput () 
+    {
+        // This is the movement supporting both "WASD" and "Arrow" keys cuz' why not.
+
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            newPosition += (transform.forward * movementSpeed);
+        }
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            newPosition += (transform.forward * -movementSpeed);
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            newPosition += (transform.right * movementSpeed);
+        }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            newPosition += (transform.forward * -movementSpeed);
+        }
+
+        // Making the movement more smooth rather then just setting the position of the transform.
+        transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime * movementTime);
     }
 }
